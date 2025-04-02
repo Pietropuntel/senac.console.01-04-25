47193a4b6d8c9ad6a85a7264cd5334f9ec724c57
Pietro Puntel <pietrosamueldossantospuntel@gmail.com>
2025-04-01 21:36:08 -0300

    coclusao do exercicio04

---
+++ aula 04/aula 04/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+double largura;
+double comprimento, area, valorMetro, valorTerreno;
+largura =  double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+valorMetro = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+area = largura * comprimento;
+
+valorTerreno = area * valorMetro;
+// saida
+Console.WriteLine("AREA -" + area.ToString("F2",CultureInfo.InvariantCulture));
+Console.WriteLine("PRECO = " + valorTerreno. ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+
+
+
+
+
+
